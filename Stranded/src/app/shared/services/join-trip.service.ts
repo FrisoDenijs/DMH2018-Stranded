4dc4c0233a0b9d5ef25b6824e135e9bc05e4a7d4
FrisoDenijs <friso1990@gmail.com>
2018-11-17 03:21:02 +0100

    added join trip funcs

--- Stranded/src/app/shared/services/join-trip.service.ts
+++ Stranded/src/app/shared/services/join-trip.service.ts
@@ -1,42 +1,22 @@
 import { Injectable } from '@angular/core';
 import { PostJoinTripRequest, PostJoinTripResponse, GetJoinTripRequest, GetJoinTripResponse, JoinTripModel } from '../models';
 import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class JoinTripService {
 
-  constructor() { }
+  private readonly joinTripUrl = 'http://172.24.23.244:8000/api/joinTrip';
+
+  constructor(private http: HttpClient) { }
 
   post(req: PostJoinTripRequest): Observable<PostJoinTripResponse> {
-    const res = new PostJoinTripResponse();
-
-    res.joinTripId = 1977;
-
-    return of<PostJoinTripResponse>(res);
+    return this.http.post<PostJoinTripResponse>(this.joinTripUrl, req);
   }
 
   get(req: GetJoinTripRequest): Observable<GetJoinTripResponse> {
-    const res = new GetJoinTripResponse();
-    const join1 = new JoinTripModel();
-    const join2 = new JoinTripModel();
-
-    res.joins = new JoinTripModel[2];
-
-    join1.age = 84;
-    join1.gender = 'vrouw';
-    join1.id = 1977;
-    join1.name = 'Annie';
-
-    join2.age = 12;
-    join2.gender = 'man';
-    join2.id = 1981;
-    join2.name = 'Puk';
-
-    res.joins[0] = join1;
-    res.joins[1] = join2;
-
-    return of<GetJoinTripResponse>();
+    return this.http.get<GetJoinTripResponse>(`${this.joinTripUrl}?trip=${req.tripId}`);
   }
 }
--- Stranded/src/app/test-services/test-services.component.ts
+++ Stranded/src/app/test-services/test-services.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { DisruptionsService, TripService } from '../shared/services';
-import { GetTripRequest, PostTripRequest } from '../shared/models';
+import { DisruptionsService, TripService, JoinTripService } from '../shared/services';
+import { GetTripRequest, PostTripRequest, PostJoinTripRequest, GetJoinTripRequest } from '../shared/models';
 
 @Component({
   selector: 'app-test-services',
@@ -11,7 +11,8 @@ export class TestServicesComponent implements OnInit {
 
   public data: any;
 
-  constructor(private disruptionService: DisruptionsService, private tripService: TripService) { }
+  constructor(private disruptionService: DisruptionsService, private tripService: TripService,
+    private joinTripService: JoinTripService) { }
 
   ngOnInit() {
   }
@@ -46,4 +47,26 @@ export class TestServicesComponent implements OnInit {
       console.log(res);
     });
   }
+
+  public postJoinTrip() {
+    const req = new PostJoinTripRequest();
+    req.age = 57;
+    req.gender = 'vrouw';
+    req.name = 'M.G. Schmidt';
+    req.tripId = 1;
+
+    this.joinTripService.post(req).subscribe(res => {
+      console.log(res);
+    });
+  }
+
+  public getJoinTrip() {
+    const req = new GetJoinTripRequest();
+    req.tripId = 1;
+
+    this.joinTripService.get(req).subscribe(res => {
+      console.log(res);
+    });
+  }
+
 }

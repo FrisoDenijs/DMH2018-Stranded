f356a5aeb3070c034d4fa85f7d57fc63e8cffcce
FrisoDenijs <friso1990@gmail.com>
2018-11-17 01:05:19 +0100

    stubbed trip service

--- Stranded/src/app/shared/models/trip.model.ts
+++ Stranded/src/app/shared/models/trip.model.ts
@@ -1,4 +1,7 @@
 export class TripModel {
     public id: number;
     public availableSeats: number;
+    public name: string;
+    public gender: string;
+    public age: number;
 }
--- Stranded/src/app/shared/services/trip.service.ts
+++ Stranded/src/app/shared/services/trip.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { PostTripRequest, PostTripResponse, GetTripRequest, GetTripResponse } from '../models';
+import { PostTripRequest, PostTripResponse, GetTripRequest, GetTripResponse, TripModel } from '../models';
 import { Observable, of } from 'rxjs';
 
 @Injectable({
@@ -10,9 +10,33 @@ export class TripService {
   constructor() { }
 
   post(req: PostTripRequest): Observable<PostTripResponse> {
-    return of<undefined>();
+    const res = new PostTripResponse();
+    res.tripId = 42;
+
+    return of<PostTripResponse>(res);
   }
   get(req: GetTripRequest): Observable<GetTripResponse> {
-    return of<undefined>();
+    const res = new GetTripResponse();
+
+    const trip1 = new TripModel();
+    const trip2 = new TripModel();
+
+    trip1.availableSeats = 2;
+    trip1.id = 42;
+    trip1.age = 19;
+    trip1.gender = 'vrouw';
+    trip1.name = 'Leia Organa';
+
+    trip2.availableSeats = 3;
+    trip2.id = 18;
+    trip1.age = 32;
+    trip1.gender = 'man';
+    trip1.name = 'Obi Wan Kenobi';
+
+    res.trips = new TripModel[2];
+    res.trips[0] = trip1;
+    res.trips[1] = trip2;
+
+    return of<GetTripResponse>(res);
   }
 }

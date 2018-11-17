3fbe3eee4b05b7bc038e1872441176d66a7a5dc1
FrisoDenijs <friso1990@gmail.com>
2018-11-17 01:11:29 +0100

    added disruption service stub

--- Stranded/src/app/shared/services/disruptions.service.ts
+++ Stranded/src/app/shared/services/disruptions.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
-import { GetDisruptionResponse } from '../models';
+import { GetDisruptionResponse, DisruptionModel } from '../models';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +10,32 @@ export class DisruptionsService {
   constructor() { }
 
   get(): Observable<GetDisruptionResponse> {
-    return of<undefined>();
+    const res = new GetDisruptionResponse();
+    const dis1 = new DisruptionModel();
+    const dis2 = new DisruptionModel();
+    const dis3 = new DisruptionModel();
+
+    res.disruptions = new DisruptionModel[3];
+
+    dis1.result = 'Koperdiefstal';
+    dis1.station = 'Amsterdam Centraal';
+    dis1.title = 'Utrecht C-Amsterdam C';
+    dis1.type = 'Storing';
+
+    dis2.result = 'Aangepaste dienstregeling';
+    dis2.station = 'Breda';
+    dis2.title = 'Eindhoven-Breda';
+    dis2.type = 'Werkzaamheid';
+
+    dis3.result = 'Aanrijding';
+    dis3.station = 'Rijswijk';
+    dis3.title = 'Den Haag C-Rijswijk';
+    dis3.type = 'Storing';
+
+    res.disruptions[0] = dis1;
+    res.disruptions[1] = dis2;
+    res.disruptions[2] = dis3;
+
+    return of<GetDisruptionResponse>(res);
   }
 }

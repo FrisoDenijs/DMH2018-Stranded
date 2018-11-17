59a1710884d87e714f7f7c9f55e332dbdfc34abf
Nathan <nathan.bruning@gmail.com>
2018-11-17 11:32:08 +0100

    Fix import

--- Stranded/src/app/shared/models/responses/index.ts
+++ Stranded/src/app/shared/models/responses/index.ts
@@ -1,5 +1,4 @@
 export * from './get-bars.response';
-export * from './get-disruptions.response';
 export * from './get-join-trip.response';
 export * from './get-trip.response';
 export * from './post-join-trip.response';

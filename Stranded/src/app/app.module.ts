798baee0cdddcdd6d2ba94f090fe319e31a2cef6
FrisoDenijs <friso1990@gmail.com>
2018-11-17 01:27:40 +0100

    implemented http call for disruptions

--- Stranded/src/app/app.module.ts
+++ Stranded/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
@@ -8,6 +9,7 @@ import { HomeComponent } from './components/home/home.component';
   declarations: [
     AppComponent,
     HomeComponent,
+    HttpClientModule,
   ],
   imports: [
     BrowserModule
--- Stranded/src/app/shared/services/disruptions.service.ts
+++ Stranded/src/app/shared/services/disruptions.service.ts
@@ -1,41 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
-import { GetDisruptionResponse, DisruptionModel } from '../models';
+import { GetDisruptionResponse } from '../models';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DisruptionsService {
 
-  constructor() { }
+  private readonly disruptionUrl = 'http://172.24.23.244:8000/api/disruptions/';
+
+  constructor(private http: HttpClient) { }
 
   get(): Observable<GetDisruptionResponse> {
-    const res = new GetDisruptionResponse();
-    const dis1 = new DisruptionModel();
-    const dis2 = new DisruptionModel();
-    const dis3 = new DisruptionModel();
-
-    res.disruptions = new DisruptionModel[3];
-
-    dis1.result = 'Koperdiefstal';
-    dis1.station = 'Amsterdam Centraal';
-    dis1.title = 'Utrecht C-Amsterdam C';
-    dis1.type = 'Storing';
-
-    dis2.result = 'Aangepaste dienstregeling';
-    dis2.station = 'Breda';
-    dis2.title = 'Eindhoven-Breda';
-    dis2.type = 'Werkzaamheid';
-
-    dis3.result = 'Aanrijding';
-    dis3.station = 'Rijswijk';
-    dis3.title = 'Den Haag C-Rijswijk';
-    dis3.type = 'Storing';
-
-    res.disruptions[0] = dis1;
-    res.disruptions[1] = dis2;
-    res.disruptions[2] = dis3;
-
-    return of<GetDisruptionResponse>(res);
+    return this.http.get<GetDisruptionResponse>(this.disruptionUrl);
   }
 }

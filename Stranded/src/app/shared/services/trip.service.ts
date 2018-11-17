6d32bd73f7a1108d5aeab85371567b975cce471d
FrisoDenijs <friso1990@gmail.com>
2018-11-17 02:05:56 +0100

    implemented trip service get

--- Stranded/src/app/shared/services/trip.service.ts
+++ Stranded/src/app/shared/services/trip.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
 import { PostTripRequest, PostTripResponse, GetTripRequest, GetTripResponse, TripModel } from '../models';
 import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TripService {
 
-  constructor() { }
+  private readonly tripUrl = 'http://172.24.23.244:8000/api/trips';
+
+  constructor(private http: HttpClient) { }
 
   post(req: PostTripRequest): Observable<PostTripResponse> {
     const res = new PostTripResponse();
@@ -15,28 +18,8 @@ export class TripService {
 
     return of<PostTripResponse>(res);
   }
+
   get(req: GetTripRequest): Observable<GetTripResponse> {
-    const res = new GetTripResponse();
-
-    const trip1 = new TripModel();
-    const trip2 = new TripModel();
-
-    trip1.availableSeats = 2;
-    trip1.id = 42;
-    trip1.age = 19;
-    trip1.gender = 'vrouw';
-    trip1.name = 'Leia Organa';
-
-    trip2.availableSeats = 3;
-    trip2.id = 18;
-    trip1.age = 32;
-    trip1.gender = 'man';
-    trip1.name = 'Obi Wan Kenobi';
-
-    res.trips = new TripModel[2];
-    res.trips[0] = trip1;
-    res.trips[1] = trip2;
-
-    return of<GetTripResponse>(res);
+    return this.http.get<GetTripResponse>(`${this.tripUrl}?to=${req.to}&from=${req.from}`);
   }
 }
